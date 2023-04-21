b2b280c01f32a6030aabde42a3b4325293c11a5b
Anupraj Varma <anupraj1854@gmail.com>
2023-04-21 16:46:21 +0530

    add image and links

---
+++ src/pages/linkes.js
@@ -0,0 +1,23 @@
+import Style from "../styles/main.module.css";
+
+export default function links() {
+  return (
+    <>
+      <div className={Style.linkeDiv}>
+        <div className={Style.linkText}>Personal Portfolio</div>
+      </div>
+      <div className={Style.linkeDiv}>
+        <div>Personal Portfolio</div>
+      </div>
+      <div className={Style.linkeDiv}>
+        <div>Personal Portfolio</div>
+      </div>
+      <div className={Style.linkeDiv}>
+        <div>Personal Portfolio</div>
+      </div>
+      <div className={Style.linkeDiv}>
+        <div>Personal Portfolio</div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/profile.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Style from "../styles/main.module.css";
+
+export default function profile() {
+  return (
+    <>
+      <Image
+        className={Style.profileImage}
+        src={"/profilePhotos.jpeg"}
+        width="130"
+        height="130"
+        alt="ProfileImage"
+      />
+      <p className={Style.myname}>Anupraj Varma</p>
+      <div className={Style.skillSec}>
+        <div className={Style.skillDiv1}>Nextjs</div>
+        <div className={Style.skillDiv2}>Javascript</div>
+        <div className={Style.skillDiv3}>Talwind CSS</div>
+        <div className={Style.skillDiv4}>Expressjs</div>
+        <div className={Style.skillDiv5}>MongoDB</div>
+      </div>
+    </>
+  );
+}
