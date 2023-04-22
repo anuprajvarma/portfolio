4d572d5316a1f317a15300fe157170529ca7bc25
Anupraj Varma <anupraj1854@gmail.com>
2023-04-22 16:08:03 +0530

    change app title and logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Profile from "./profile";
 import Links from "./linkes";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Anupraj Verma</title>
+      </Head>
       <Profile />
       <Links />
     </>
--- src/pages/linkes.js
+++ src/pages/linkes.js
@@ -1,23 +1,47 @@
+import Link from "next/link";
+import { FaGithubAlt } from "react-icons/fa";
+import { BsChatRightTextFill } from "react-icons/bs";
+import { useRouter } from "next/router";
 import Style from "../styles/main.module.css";
 
 export default function links() {
+  const router = useRouter();
   return (
     <>
       <div className={Style.linkeDiv}>
-        <div className={Style.linkText}>Personal Portfolio</div>
+        <Link href="https://anuprajvarma.vercel.app/" target="_blank">
+          <div className={Style.linkText1}>Personal Portfolio</div>
+        </Link>
       </div>
       <div className={Style.linkeDiv}>
-        <div>Personal Portfolio</div>
+        <Link
+          href="https://www.linkedin.com/in/anupraj-varma-a04489208/"
+          target="_blank"
+        >
+          <div className={Style.linkText2}>Connect to me on Linkedin</div>
+        </Link>
       </div>
       <div className={Style.linkeDiv}>
-        <div>Personal Portfolio</div>
+        <Link href="https://github.com/anuprajvarma" target="_blank">
+          <div className={Style.linkText3}>Follow me on GitHub</div>
+        </Link>
       </div>
       <div className={Style.linkeDiv}>
-        <div>Personal Portfolio</div>
+        <Link href="https://twitter.com/Anupraj_varma" target="_blank">
+          <div className={Style.linkText4}>
+            Let's all laugh together at tech Twitter memes
+          </div>
+        </Link>
       </div>
       <div className={Style.linkeDiv}>
-        <div>Personal Portfolio</div>
+        <Link href="https://www.instagram.com/anupraj_verma/" target="_blank">
+          <div className={Style.linkText5}>Instagram</div>
+        </Link>
       </div>
+      <BsChatRightTextFill
+        className={Style.icon}
+        onClick={() => router.push("mailto:anupraj1854@gmail.com")}
+      />
     </>
   );
 }
