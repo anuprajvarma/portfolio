03aeedbd015545a3f42adad13cb642e1730bc9da
anuprajvarma <anupraj1854@gmail.com>
2025-03-11 16:00:17 +0530

    add Read more button in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Header from "@/components/Header";
 import Home from "@/components/Home";
 
 export default function Page() {
   return (
     <div className="w-6/12 m-auto">
-      <Header/>
-      <Home/>
+      <Home />
     </div>
   );
 }
--- components/Home.tsx
+++ components/Home.tsx
@@ -5,15 +5,16 @@ import {
   PiTwitterLogoLight,
   PiYoutubeLogoLight,
 } from "react-icons/pi";
+import { MdKeyboardDoubleArrowDown } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div className="p-4 gap-8 flex flex-col w-full">
+    <div className="p-4 py-10 gap-12 flex flex-col w-full">
       <div className="flex justify-between w-full">
         <div className="w-9/12 flex flex-col gap-3">
-          <h1 className="text-3xl font-bold">Anupraj Varma</h1>
+          <h1 className="text-4xl font-bold">Anupraj Varma</h1>
           <div className="flex flex-col gap-4">
-            <p>
+            <p className="text-sm">
               I’m a software engineer and CEO of Vercel. I’m originally from
               Lanús, Buenos Aires, Argentina. I owe much of my career to the Web
               and open source.
@@ -26,13 +27,13 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div>
+        <div className="h-[7rem] w-[7rem]">
           <Image
             className="rounded-full"
             src="/portfolio_pic.png"
             alt="Portfolio pic"
-            width={100}
-            height={100}
+            width={50}
+            height={50}
             layout="responsive"
           />
         </div>
@@ -75,6 +76,12 @@ const Home = () => {
               This is a writeup based on a presentation I gave at BrazilJS in
               August 2014....
             </p>
+          </div>
+          <div className="py-8 cursor-pointer w-full flex items-center flex-col">
+            <div className="w-4/12 flex flex-col justify-center items-center text-center">
+              <h1 className="text-2xl font-medium">Read More</h1>
+              <MdKeyboardDoubleArrowDown size={30} />
+            </div>
           </div>
         </div>
       </div>
