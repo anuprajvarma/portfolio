4fc50577ab288095f8e3028de2be65a28681077d
anuprajvarma <anupraj1854@gmail.com>
2025-03-17 02:44:24 +0530

    add drive link of resume in resume page

---
+++ app/resume/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <div className="w-full">
+      <div className="w-[40rem] m-auto">
+        <iframe
+          src="https://drive.google.com/file/d/18rfJCbbjdzuMDO3Sj33Z9sm8fkgUW74U/preview"
+          allow="autoplay"
+          className="w-full h-[45rem]"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
