57f01d6b820dd52fe8451d0fc0bee975b8d3cd0a
anuprajvarma <anupraj1854@gmail.com>
2025-03-17 02:58:29 +0530

    made frontpage of blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,75 @@
 export default function BlogPost() {
-  return <h1>Blog Post</h1>;
+  return (
+    <div className="w-full">
+      <div className="w-[40rem] m-auto flex flex-col gap-4">
+        <div className="border-y border-grey-light p-2 rounded-md">
+          <p className="text-lg">8 post in total</p>
+        </div>
+        <div className="flex flex-col cursor-pointer p-2">
+          <div className="py-4 border-b border-grey-light hover:bg-grey rounded-md">
+            <div className="flex justify-between">
+              <h2 className="text-xl font-medium">
+                7 Principles of Rich Web Applications
+              </h2>
+              <p>10 Mar, 2025</p>
+            </div>
+            <p className="font-medium">
+              This is a writeup based on a presentation I gave at BrazilJS in
+              August 2014....
+            </p>
+          </div>
+          <div className="py-4 border-b border-grey-light hover:bg-grey rounded-md">
+            <div className="flex justify-between">
+              <h2 className="text-xl font-medium">
+                7 Principles of Rich Web Applications
+              </h2>
+              <p>10 Mar, 2025</p>
+            </div>
+            <p className="font-medium">
+              This is a writeup based on a presentation I gave at BrazilJS in
+              August 2014....
+            </p>
+          </div>
+          <div className="py-4 border-b border-grey-light hover:bg-grey rounded-md">
+            <div className="flex justify-between">
+              <h2 className="text-xl font-medium">
+                7 Principles of Rich Web Applications
+              </h2>
+              <p>10 Mar, 2025</p>
+            </div>
+            <p className="font-medium">
+              This is a writeup based on a presentation I gave at BrazilJS in
+              August 2014....
+            </p>
+          </div>
+          <div className="py-4 border-b border-grey-light hover:bg-grey rounded-md">
+            <div className="flex justify-between">
+              <h2 className="text-xl font-medium">
+                7 Principles of Rich Web Applications
+              </h2>
+              <p>10 Mar, 2025</p>
+            </div>
+            <p className="font-medium">
+              This is a writeup based on a presentation I gave at BrazilJS in
+              August 2014....
+            </p>
+          </div>
+          <div className="relative w-full flex justify-center">
+            <div className="py-4 border-b w-full border-grey-light rounded-md hover:bg-grey ">
+              <div className="flex justify-between">
+                <h2 className="text-xl font-medium">
+                  7 Principles of Rich Web Applications
+                </h2>
+                <p>10 Mar, 2025</p>
+              </div>
+              <p className="font-medium">
+                This is a writeup based on a presentation I gave at BrazilJS in
+                August 2014....
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
