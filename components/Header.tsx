5b24d6a920dc23a1581d7043ca9c039f30698d03
anuprajvarma <anupraj1854@gmail.com>
2025-03-29 20:12:31 +0530

    fix: make current header text bold

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,43 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <header className="w-full">
       <div className="sm:w-[40rem] w-full m-auto flex justify-between items-center px-2">
         <nav className={`cursor-pointer w-full flex`}>
           <ul className="flex text-2xl w-full justify-between sm:justify-start flex-row gap-4">
-            <li className="hover:bg-[var(--bg)] rounded-md p-2">
+            <li
+              className={`hover:bg-[var(--bg)] rounded-md p-2 ${
+                pathname == "/" ? "font-bold" : "font-normal"
+              }`}
+            >
               <Link href="/">Home</Link>
             </li>
-            <li className="hover:bg-[var(--bg)] rounded-md p-2">
+            <li
+              className={`hover:bg-[var(--bg)] rounded-md p-2 ${
+                pathname == "/about" ? "font-bold" : "font-normal"
+              }`}
+            >
               <Link href="/about">About</Link>
             </li>
-            <li className="hover:bg-[var(--bg)] rounded-md p-2">
+            <li
+              className={`hover:bg-[var(--bg)] rounded-md p-2 ${
+                pathname == "/blog" ? "font-bold" : "font-normal"
+              }`}
+            >
               <Link href="/blog">Blog</Link>
             </li>
-            <li className="hover:bg-[var(--bg)] rounded-md p-2">
+            <li
+              className={`hover:bg-[var(--bg)] rounded-md p-2 ${
+                pathname == "/resume" ? "font-bold" : "font-normal"
+              }`}
+            >
               <Link href="/resume">Resume</Link>
             </li>
           </ul>
